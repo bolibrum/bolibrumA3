faa2520109be3c88c8f0d8f72dda36915c74ce54
Mark <52496936+bolibrum@users.noreply.github.com>
2019-07-19 00:50:33 -0400

    ANGULAR github files
    First iteration

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,9 @@
+import { books } from '../../app/books';
+
+export const MYBOOKS: books[] = [
+    { bName: "Romeo and Juliet", bAuthor: "William Shakespeare", bGenre: "Tragic Romance", bYearPublished: "1595", bPicture: "assets/images/Romeo_and_juliet.jpg" },
+    { bName: "1984", bAuthor: "George Orwell", bGenre: "Science Fiction", bYearPublished: "1949", bPicture: "assets/images/1984.jpg" },
+    { bName: "History of the Necronomicon", bAuthor: "H.P Lovecraft", bGenre: "Horror", bYearPublished: "1938", bPicture: "assets/images/necronomicon.jpg" },
+    { bName: "Alice in Wonderland", bAuthor: "Lewis Carroll", bGenre: "Fantasy", bYearPublished: "1865", bPicture: "assets/images/aliceinwonderland.jpg" },
+    { bName: "War and Peace", bAuthor: "Leo Tolstoy", bGenre: "History", bYearPublished: "1869", bPicture: "assets/images/warandpeace.jpg" }
+] 
